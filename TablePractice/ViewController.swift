44c34295c8324c3c0bfb65fadb79ffb53cec5b31
RedTyke <john.mcniffe@hotmail.com>
2018-05-22 21:20:31 +0100

    Initial Commit

---
+++ TablePractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TablePractice
+//
+//  Created by John McNiffe on 22/05/2018.
+//  Copyright © 2018 John McNiffe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
